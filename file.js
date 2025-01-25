6299aa8e64b19204d0f3a220229eae8b5b2d1354
zaakiraza <raaza.zaakir@gmail.com>
2025-01-25 12:43:28 +0500

    toggle problem

--- file.js
+++ file.js
@@ -1,31 +1,33 @@
-// let inn=document.getElementById("inn");
-// let img=document.getElementById("img");
-// inn.onchange=function(){
-//   img.src=URL.createObjectURL(inn.files[0]);
-// }
-
+// 3 dots box open
 let box = document.getElementById("box");
 function boxAppear(){
     box.style.display="block";
-}
-function boxdisAppear() {
-    box.addEventListener("onhover", box.style.display = "none");
+    document.getElementById('overlay').style.display='block';
 }
 
+// Gear box open
+function openmenu() {
+    document.getElementById('openRotate').style.transform="rotate(90deg)";
+    document.getElementById('openRotate').style.transition="1s";
+    document.getElementById("setting_option").style.display = "block";
+    document.getElementById('overlay').style.display='block';
+}
+
+// Close boxes
+document.getElementById('overlay').addEventListener("click",()=>{
+    box.style.display="none";
+    document.getElementById('openRotate').style.transition="1s";
+    document.getElementById('openRotate').style.transform="rotate(-90deg)";
+    document.getElementById("setting_option").style.display="none";
+});
+
+// LogIn Appear
 function toggleup() {
     document.getElementById("box_signup").style.display = "none";
     document.getElementById("box_login").style.display = "block";
 }
+// SignUp Appear
 function toggledown() {
     document.getElementById("box_login").style.display = "none";
     document.getElementById("box_signup").style.display = "block";
 }
-
-let menu = document.getElementById("setting_option");
-function openmenu() {
-    menu.style.display = "block";
-}
-function closemenu() {
-    menu.style.display = "none";
-}
-
